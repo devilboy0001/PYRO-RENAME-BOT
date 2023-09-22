dca04f693b681d6a975cf2cd82b4468e091ff6ca
devilboy0001 <141001196+devilboy0001@users.noreply.github.com>
2023-09-22 13:37:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,20 +26,20 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     # pyro client config
-    API_ID    = os.environ.get("API_ID", "")
-    API_HASH  = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    API_ID    = os.environ.get("API_ID", "29696092")
+    API_HASH  = os.environ.get("API_HASH", "58d9ba5b26ff3b50fbb811d64c077541")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6503758212:AAFR_g8oWQCSTUlm3E-kWART1z4bvVYJ23E") 
    
     # database config
-    DB_NAME = os.environ.get("DB_NAME","pyro-botz")     
-    DB_URL  = os.environ.get("DB_URL","")
+    DB_NAME = os.environ.get("DB_NAME","Cluster0")     
+    DB_URL  = os.environ.get("DB_URL","mongodb+srv://devilboy_edit:devilboy555@cluster0.el9nwqs.mongodb.net/?retryWrites=true&w=majority")
  
     # other configs
     BOT_UPTIME  = time.time()
-    START_PIC   = os.environ.get("START_PIC", "")
-    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
-    FORCE_SUB   = os.environ.get("FORCE_SUB", "") 
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", None))
+    START_PIC   = os.environ.get("START_PIC", "https://graph.org/file/f64cd2728c3d57d244bad.jpg")
+    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5263032279').split()]
+    FORCE_SUB   = os.environ.get("FORCE_SUB", "devilboy_edit") 
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "dbrenamer"))
 
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", "True"))
